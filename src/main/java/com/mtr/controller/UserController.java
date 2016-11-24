b60e97ce6238535c1e6eb4eba05a3e7dd1489589
Scarb <jjhfen00@163.com>
2016-11-24 15:33:50 +0800

    Update to have json response, failed.

--- src/main/java/com/mtr/controller/UserController.java
+++ src/main/java/com/mtr/controller/UserController.java
@@ -2,16 +2,24 @@ package com.mtr.controller;
 
 import com.mtr.model.User;
 import com.mtr.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 
 /**
  * Created by Scarb's Surface on 11/22/2016.
  */
-@Controller
+@RestController
 public class UserController {
     @Resource
     private UserService userService;
@@ -23,5 +31,18 @@ public class UserController {
         User user = userService.selectUserById(1);
         mav.addObject("user", user);
         return mav;
+    }
+
+    @ResponseBody
+    @RequestMapping("/responseEntity")
+    public List<User> responseEntityTest()
+    {
+        Dictionary<String, String> dictionary = new Hashtable<String, String>();
+        dictionary.put("Status", "true");
+        User user = new User();
+        user.setId(1234);
+        List<User> testList = new ArrayList<>();
+        testList.add(user);
+        return testList;
     }
 }
